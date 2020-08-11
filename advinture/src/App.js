94aaac79d617a187fd7fafd1ae686a67186b4920
Alexander Miesen <alexmiesen@hotmail.com>
2020-08-11 20:06:46 +0200

    added bootstrap and fontawesome

--- advinture/src/App.js
+++ advinture/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import caro_frida from './images/caro_frida.jpg'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Button from './components/button'
 
 function App() {
   return (
-    <div className="Does">
+    <div className="container">
       <header className="App-header">
         <img src={caro_frida} clasName="caro-frida" alt="caro and frida" />
         <Button>
--- advinture/src/components/button.js
+++ advinture/src/components/button.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faWineGlassAlt} from '@fortawesome/free-solid-svg-icons'
 
 const Button = () => {
   return (
     <div>
       <button type="button">
+        <FontAwesomeIcon icon={faWineGlassAlt}/>
         Info
       </button>
 
